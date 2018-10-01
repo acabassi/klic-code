628afefe8f5f02520bc4cef2c91beadf18d44c31
Alessandra Cabassi <ac2051@cam.ac.uk>
2018-10-01 17:52:44 +0100

    fewer datasets

--- synthetic-data/synthetic-data-a.R
+++ synthetic-data/synthetic-data-a.R
@@ -10,11 +10,12 @@ library(ggplot2)
 library(GGally)
 library(reshape)
 
-### Six clusters, same cluster separability in each dataset
+### Data generation ### 
+### A. Six clusters, same cluster separability in each dataset ###
 
 n_experiments <- 100
 n_separation_levels <- 1
-n_datasets_same_rho <- 5
+n_datasets_same_rho <- 3
 n_variables <- 2
 n_obs_per_cluster <- 50
 n_clusters <- 6
@@ -42,6 +43,8 @@ for(i in 1:n_datasets_same_rho){
 uno <- rep(1, n_obs_per_cluster)
 cluster_labels <- c(uno, uno*2, uno*3, uno*4, uno*5, uno*6)
 
+### Plot ###
+
 dataset_example <- data[,,1,1]
 example6_dataframe <- as.data.frame(dataset_example)
 example6_dataframe$cluster <- as.factor(cluster_labels)
@@ -52,3 +55,46 @@ ggpairs(data=example6_dataframe, # data.frame with variables
 ggsave("synthetic-data-a.pdf", width = 7, height = 7)
 
 save(data, file = "synthetic-data-a.RData")
+
+### One at a time ###
+
+library(coca)
+
+CM <- array(NA, c(n_obs_per_cluster*n_clusters, n_obs_per_cluster*n_clusters,
+                  n_datasets_same_rho, n_experiments))
+ari_one <- array(NA, c(n_datasets_same_rho, n_experiments))
+
+parameters <- list()
+parameters$cluster_count <- n_clusters
+
+for(i in 1:n_datasets_same_rho){
+  for(j in 1:n_experiments){
+    CM_temp <- consensusCluster(data[,,i,j], n_clusters)
+    CM[,,i,j] <- spectrumShift(CM_temp)
+    kkmeans_labels <- kkmeansTrain(CM[,,i,j], parameters)$clustering
+    ari_one[i,j] <- adjustedRandIndex(kkmeans_labels, cluster_labels)
+  }
+}
+
+### All together ###
+
+library(klic)
+library(mclust)
+
+ari_all <- rep(NA, n_experiments)
+weights <- array(NA, c(n_datasets_same_rho, n_experiments))
+
+data_for_klic <- list()
+for(j in 1:n_experiments){
+  for(i in 1:n_datasets_same_rho){
+    data_for_klic[[i]] <- data[,,i,j]
+  }
+  klicOutput <- klic(data_for_klic, n_datasets_same_rho,
+                      individualK = rep(n_clusters, n_datasets_same_rho),
+                      globalK = n_clusters)
+  klic_labels <- klicOutput$globalClusterLabels
+  weights[,j] <- klicOutput$weights
+  ari_all[j] <- adjustedRandIndex(klic_labels, cluster_labels) 
+}
+
+save(ari_one, ari_all, weights, file = "ari-a.RData")

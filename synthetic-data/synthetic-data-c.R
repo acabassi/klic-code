3e4ec6e9fe5aa8c1a422fc2d46575dbe36497d0f
Alessandra Cabassi <ac2051@cam.ac.uk>
2019-01-29 14:30:39 +0000

    update code for simulation C

--- synthetic-data/synthetic-data-c.R
+++ synthetic-data/synthetic-data-c.R
@@ -15,10 +15,20 @@ library(mclust)
 library(reshape2)
 library(ggplot2)
 
+# Define ggplot2 theme 
+my_theme <-  theme(
+    panel.background = element_rect(fill = NA),
+    panel.grid.major = element_line(colour = "grey50"),
+    panel.grid.major.x = element_blank() ,
+    panel.grid.major.y = element_line(size=.1, color="black"),
+    axis.ticks.y = element_blank(),
+    axis.ticks.x = element_blank()
+)
+
 ### Data generation ### 
 ### C1. Six clusters ###
 
-n_experiments <- 100
+n_experiments <- 13
 n_separation_levels <- 1
 n_types <- 3
 n_variables <- 2
@@ -67,7 +77,8 @@ example6_dataframe <- as.data.frame(dataset_example)
 example6_dataframe$cluster <- as.factor(cluster_labels)
 ggpairs(data=example6_dataframe, # data.frame with variables
         title="6 clusters",  # title of the plot
-        mapping=ggplot2::aes(colour = cluster))
+        mapping=ggplot2::aes(colour = cluster),
+        lower=list(combo=wrap("facethist", binwidth=0.8)))
 ggsave("synthetic-data-c1.pdf", width = 7, height = 7)
 
 t = 2 # 3 clusters
@@ -76,7 +87,8 @@ example3_dataframe <- as.data.frame(dataset_example)
 example3_dataframe$cluster <- as.factor(cluster_labels)
 ggpairs(data=example3_dataframe, # data.frame with variables
         title="3 clusters", # title of the plot
-        mapping = ggplot2::aes(colour= cluster))
+        mapping = ggplot2::aes(colour= cluster),
+        lower=list(combo=wrap("facethist", binwidth=0.8)))
 ggsave("synthetic-data-c2.pdf", width = 7, height = 7)
 
 t = 3 # 6 clusters, well separated
@@ -85,14 +97,15 @@ example6plus_dataframe <- as.data.frame(dataset_example)
 example6plus_dataframe$cluster <- as.factor(cluster_labels)
 ggpairs(data=example6plus_dataframe, # data.frame with variables
         title="6 clusters, well separated", # title of the plot
-        mapping = ggplot2::aes(colour= cluster))
+        mapping = ggplot2::aes(colour= cluster),
+        lower=list(combo=wrap("facethist", binwidth=0.8)))
 ggsave("synthetic-data-c3.pdf", width = 7, height = 7)
 
 save(data, file = "synthetic-data-c.RData")
 
 ### Clustering one dataset at a time ###
 
-ari_one <- array(NA, c(n_separation_levels, n_experiments))
+ari_one <- array(NA, c(n_types, n_experiments))
 
 # Initialise parameters for kernel k-means
 parameters <- list()
@@ -151,30 +164,50 @@ for(j in 1:n_experiments){
 save(ari_one, ari_all, weights, file = "ari-c.RData")
 
 
-# # Load results
-# load("ari-c.RData")
-# 
-# dim(ari_one)
-# dim(as.matrix(ari_all))
-# ari <- cbind(t(ari_one), t(ari_all))
-# colnames(ari) <- c("0", "1", "2", "3", "0+1+2", "0+1+3", "0+2+3", "1+2+3")
-# 
-# ari.m <- melt(ari)
-# ari.m # pasting some rows of the melted data.frame
-# 
-# ggplot(data = ari.m, aes(x=X2, y=value)) + geom_boxplot() + ylim(0,1)
-# ggsave("ari-c.pdf")
-# 
-# dim(weights)
-# 
-# dimnames(weights) <- list(c("1st", "2nd", "3rd"), c("0+1+2", "0+1+3", "0+2+3", "1+2+3"), 1:100)
-# 
-# labels <- matrix(data = c("0","1","2","0","1","3","0","2","3","1","2","3"), nrow = 4, ncol = 3, byrow = TRUE)
-# for(i in 1:4){
-#   weights_i <- weights[,i,]
-#   rownames(weights_i) <- labels[i,]
-#   weights.m <- melt(t(weights_i))
-#   ggplot(data = weights.m, aes(x=as.character(X2), y=value)) + geom_boxplot() + ylim(0,1)
-#   ggsave(paste("weights-c",as.character(i),".pdf", sep=""))
-# }
-# 
+# Load results
+load("ari-c.RData")
+
+dim(ari_one)
+dim(as.matrix(ari_all))
+ari <- cbind(t(ari_one), t(ari_all))
+colnames(ari) <- c("6", "3", "6*", "3+6", "3+6*")
+
+ari.m <- melt(ari)
+head(ari.m) # pasting some rows of the melted data.frame
+colnames(ari.m) <- c("Experiment", "Datasets", "ARI")
+
+ari.m$Datasets <- factor(ari.m$Datasets,
+                       levels = c("3","6", "6*", "3+6", "3+6*"), ordered = TRUE)
+
+ggplot(data = ari.m, aes(x=Datasets, y=ARI)) + geom_boxplot() + ylim(0,1) + theme(
+    panel.background = element_rect(fill = NA),
+    panel.grid.major = element_line(colour = "grey50"),
+    panel.ontop = TRUE
+)
+ggsave("ari-c.pdf")
+
+dim(weights)
+ 
+weights_1 <- weights[,1,]
+weights_2 <- weights[,2,]
+
+rownames(weights_1) <- c("6", "3")
+rownames(weights_2) <- c("3", "6*")
+colnames(weights_1) <- colnames(weights_2) <- 1:n_experiments
+
+weights_1.m <- melt(t(weights_1))
+head(weights_1.m) 
+colnames(weights_1.m) <- c("Experiments", "Dataset", "Weight")
+weights_1.m$Dataset <- factor(weights_1.m$Dataset,
+                         levels = c("3","6"), ordered = TRUE)
+
+
+ggplot(data = weights_1.m, aes(x=Dataset, y=Weight)) + geom_boxplot() + ylim(0,1) + my_theme
+ggsave(paste("weights-c1.pdf", sep=""))
+
+weights_2.m <- melt(t(weights_2))
+head(weights_2.m) 
+colnames(weights_2.m) <- c("Experiments", "Dataset", "Weight")
+
+ggplot(data = weights_2.m, aes(x=Dataset, y=Weight)) + geom_boxplot() + ylim(0,1) + my_theme
+ggsave(paste("weights-c2.pdf", sep=""))

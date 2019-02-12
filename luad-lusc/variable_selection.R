23c1fb7d57a1685244dbae113d3479e07484ce5e
Alessandra Cabassi HPC <ac2051@cam.ac.uk>
2019-02-12 14:49:45 +0000

    compute 100 protoclusts

--- luad-lusc/variable_selection.R
+++ luad-lusc/variable_selection.R
@@ -3,7 +3,7 @@
 ########################################
 
 rm(list=ls())
-setwd("~/OneDrive - University of Cambridge, MRC Biostatistics Unit/PHD-PROJECTS/klic-code/luad-lusc/")
+setwd("/home/ac2051/rds/hpc-work/klic-code/luad-lusc/")
 
 load("merged-data.RData")
 
@@ -15,12 +15,12 @@ dist_RNAseq <- dist(t(lu$RNA_seq), method = "euclidean")
 hc_RNAseq <- protoclust(dist_RNAseq)
 cut_RNAseq <- protocut(hc_RNAseq, k = K)
 
-dist_mutation <- dist(lu$mutation, method = "binary")
+dist_mutation <- dist(t(lu$mutation), method = "binary")
 hc_mutation <- protoclust(dist_mutation)
 cut_mutation <- protocut(hc_mutation, k = K)
 
-dist_miRNAseq <- dist(lu$miRNAseq, method = "euclidean")
+dist_miRNAseq <- dist(t(lu$miRNAseq), method = "euclidean")
 hc_miRNAseq <- protoclust(dist_miRNAseq)
 cut_miRNAseq <- protocut(hc_miRNAseq, k = K)
 
-save(cut_RNAseq, cut_mutation, cut_miRNAseq, file = "cut_protoclusts.R")
+save(cut_RNAseq, cut_mutation, cut_miRNAseq, file = "cut_protoclusts.RData")
